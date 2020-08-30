ddf314a7bffd12274d8018c702e2e5411758b86a
Aryan1225 <70463625+Aryan1225@users.noreply.github.com>
2020-08-30 12:32:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+                     var counter = 0;
+
+function up() {
+ counter =  1;
+
+ document.getElementById("up").innerHTML = counter + " Like";
+}
+             
+function down() {
+ counter = - 1;
+
+ document.getElementById("up").innerHTML = counter + " Like";
+};
+          
+                   
+                                        var ctr = 0;
+
+function Like() {
+ ctr =  1;
+
+ document.getElementById("Like").innerHTML = ctr + " Like";
+}
+             
+function Dislike() {
+ ctr = - 1;
+
+ document.getElementById("Like").innerHTML = ctr + " Like";
+};
+                   
+                                                         var cpr = 0;
+
+function good() {
+ cpr =  1;
+
+ document.getElementById("good").innerHTML = cpr + " Like";
+}
+             
+function bad() {
+ cpr = - 1;
+
+ document.getElementById("good").innerHTML = cpr + " Like";
+}
+                   
+                   
+                                                         var cp = 0;
+
+function a() {
+ cp =  1;
+
+ document.getElementById("a").innerHTML = cp + " Like";
+}
+             
+function b() {
+ cp = -1;
+
+ document.getElementById("a").innerHTML = cp + " Like";
+}
+                                                           var c = 0;
+
+function n() {
+ c =  1;
+
+ document.getElementById("n").innerHTML = c + " Like";
+}
+             
+function m() {
+ c = -1;
+
+ document.getElementById("n").innerHTML = c + " Like";
+}        
+  var z = 0;
+
+function x() {
+ z =  1;
+
+ document.getElementById("x").innerHTML = z + " Like";
+}
+             
+function y() {
+ z = -1;
+
+ document.getElementById("x").innerHTML = z + " Like";
+}   
